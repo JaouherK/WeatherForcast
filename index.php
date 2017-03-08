c7a654aa9771cf0542c68b9cf5cb377b523d84b2
Jaouher <kharratjaouher@gmail.com>
2017-03-08 04:06:18 +0100

    modifications

--- index.php
+++ index.php
@@ -1,150 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Weather Forecast</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+
+    <style type="text/css">
+        body {
+            text-align: center;
+        }
+
+        #city {
+            width: 200px;
+            height: 30px;
+            font-size: 16px;
+        }
+
+        #btn {
+            width: 80px;
+            height: 30px;
+            font-size: 16px;
+        }
+
+        #weather {
+            font-size: 20px;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+
+
+<input class="form-controller" id="city" name="city">
+        <input id="btn" type="submit" value="Go!"/>
+    </form>
+</div>
+
+
 <?php
 /**
  * Created by PhpStorm.
  * User: kharr
  * Date: 07/03/2017
- * Time: 21:52
+ * Time: 23:04
  */
+if (isset($_POST['city'])) {
+    $check = checkCityOrCountry($_POST['city']);
 
+    if (!$check) {
+        ?>
+        <div class="alert alert-warning">
+            <strong>Warning!</strong> This is not a valid address.
+        </div>
+<?php
+    }
+    else {
+$i =0;
+        foreach ($check as $alternative) {
+            echo $i++."/ ";
+            foreach ($alternative['address_components'] as $alt) {
+                if (in_array('political',$alt['types'] ))
+                echo "<a href='getForecast.php?place=".$alt['long_name']."'>".$alt['long_name']."</a> (".$alt['types'][0].") > ";
+            }
+            echo "<br>";
+        }
+    }
+}
 
-/**
- * OpenWeatherMap-PHP-API — A php api to parse weather data from http://www.OpenWeatherMap.org .
- *
- * @license MIT
- *
- * Please see the LICENSE file distributed with this source code for further
- * information regarding copyright and licensing.
- *
- * Please visit the following links to read about the usage policies and the license of
- * OpenWeatherMap before using this class:
- *
- * @see http://www.OpenWeatherMap.org
- * @see http://www.OpenWeatherMap.org/terms
- * @see http://openweathermap.org/appid
- */
-
-use Cmfcmf\OpenWeatherMap;
-use Cmfcmf\OpenWeatherMap\AbstractCache;
-
-
-require_once '/Controller/bootstrap.php';
-
-/**
- * Example cache implementation.
- *
- * @ignore
- */
-class ExampleCache extends AbstractCache
+function checkCityOrCountry($name)
 {
-    protected $tmp;
+    $geo_data = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.$name.'&sensor=true&language=en');
+    $arr = json_decode($geo_data, true);
 
-    public function __construct()
-    {
-        $this->tmp = sys_get_temp_dir();
-    }
-
-    private function urlToPath($url)
-    {
-        $dir = $this->tmp . DIRECTORY_SEPARATOR . "OpenWeatherMapPHPAPI";
-        if (!is_dir($dir)) {
-            mkdir($dir);
-        }
-
-        $path = $dir . DIRECTORY_SEPARATOR . md5($url);
-
-        return $path;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function isCached($url)
-    {
-        $path = $this->urlToPath($url);
-        if (!file_exists($path) || filectime($path) + $this->seconds < time()) {
-            echo "Weather data is NOT cached!\n";
-
-            return false;
-        }
-
-        echo "Weather data is cached!\n";
-
-        return true;
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function getCached($url)
-    {
-        return file_get_contents($this->urlToPath($url));
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function setCached($url, $content)
-    {
-        file_put_contents($this->urlToPath($url), $content);
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function setTempPath($path)
-    {
-        if (!is_dir($path)) {
-            mkdir($path);
-        }
-
-        $this->tmp = $path;
-    }
+    if ($arr['status'] == 'ZERO_RESULTS')
+    return false;
+    else
+        return $arr['results'];
 }
-
-// Language of data (try your own language here!):
-$lang = 'en';
-
-// Units (can be 'metric' or 'imperial' [default]):
-$units = 'metric';
-
-// Example 1: Use your own cache implementation. Cache for 10 seconds only in this example.
-$cache = new ExampleCache();
-$cache->setTempPath(__DIR__.'/temps');
-$owm = new OpenWeatherMap($myApiKey, null, $cache, 10);
-
-$forecast = $owm->getWeatherForecast('Berlin', $units, $lang, '', 10);
-echo "EXAMPLE 1<hr />\n\n\n";
-
-echo "City: " . $forecast->city->name;
-echo "<br />\n";
-echo "LastUpdate: " . $forecast->lastUpdate->format('d.m.Y H:i');
-echo "<br />\n";
-echo "Sunrise : " . $forecast->sun->rise->format("H:i:s (e)") . " Sunset : " . $forecast->sun->set->format("H:i:s (e)");
-echo "<br />\n";
-echo "<br />\n";
-
-foreach ($forecast as $weather) {
-    // Each $weather contains a Cmfcmf\ForecastWeather object which is almost the same as the Cmfcmf\Weather object.
-    // Take a look into 'Examples_Current.php' to see the available options.
-    echo "Weather forecast at " . $weather->time->day->format('d.m.Y') . " from " . $weather->time->from->format('H:i') . " to " . $weather->time->to->format('H:i');
-    echo "<br />\n";
-    echo $weather->temperature;
-    echo "<br />\n";
-    echo "Sun rise: " . $weather->sun->rise->format('d.m.Y H:i (e)');
-    echo "<br />\n";
-    echo "---";
-    echo "<br />\n";
-}
-
-// Example 2: Get forecast for the next 3 days for Berlin.
-$forecast = $owm->getWeatherForecast('Berlin', $units, $lang, '', 3);
-echo "EXAMPLE 2<hr />\n\n\n";
-
-foreach ($forecast as $weather) {
-    echo "Weather forecast at " . $weather->time->day->format('d.m.Y') . " from " . $weather->time->from->format('H:i') . " to " . $weather->time->to->format('H:i') . "<br />";
-    echo $weather->temperature . "<br />\n";
-    echo "<br />\n";
-    echo "Sun rise: " . $weather->sun->rise->format('d.m.Y H:i (e)');
-    echo "<br />\n";
-    echo "---<br />\n";
-}
+?>
+</body>
+</html>
